5eef6613031d65aad3caf5c8e061b80fc67fd7c6
TysonLevy <levytj@mail.uc.edu>
2024-11-23 11:25:44 -0500

    second pass of checkout/bookings details page

--- src/components/checkout/checkout.jsx
+++ src/components/checkout/checkout.jsx
@@ -3,8 +3,8 @@ import "./checkout.css"
 
 export function Checkout() {
     let exampleBooking = {
-        startDate:'2024-11-16',
-        endDate:'2024-11-17',
+        startDate:'2024-11-25',
+        endDate:'2024-11-26',
         name: 'Tyson Levy',
         hotelAddress: "1234 Hotel Ave",
         hotelName: "Test Hotel Suites",
@@ -16,11 +16,18 @@ export function Checkout() {
     const [name, setName] = useState(exampleBooking.name)
     const [numberAdults, setNumberAdults] = useState(exampleBooking.numberAdults)
     const [numberChildren, setNumberChildren] = useState(exampleBooking.numberChildren)
+    const [startDate, setStartDate] = useState(exampleBooking.startDate)
+    const [endDate, setEndDate] = useState(exampleBooking.endDate)
+    const [roomSize, setRoomSize] = useState(exampleBooking.roomSize)
 
-    const today = new Date()
+    const todayDate = new Date()
     const oneDay = 1000 * 60 * 60 * 24;
-    const diffInTime = new Date(exampleBooking.endDate).getTime() - new Date(exampleBooking.startDate).getTime();
+    const diffInTime = new Date(endDate).getTime() - new Date(startDate).getTime();
     const diffInDays = Math.round(diffInTime / oneDay);
+    const today = todayDate.toISOString().split('T')[0]
+
+
+    let totalCost = (exampleBooking.baseRate * (numberAdults + (numberChildren * .5)) * diffInDays) * (roomSize === "small" ? 1 : roomSize === "medium" ? 1.25 : 1.5)
     
     return (
     <div className='bookingDetails'>
@@ -33,11 +40,28 @@ export function Checkout() {
                 <div className='formRow'>
                         <div className="input">
                             <label className="form-label">Start</label>
-                            <input type="date" defaultValue={exampleBooking.startDate} min={today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate()}></input>
+                            <input type="date" defaultValue={startDate} required="required" min={today} max={minusDay(endDate)} onChange={(e) => {
+                                if(e.target.value!=="") {
+                                    setStartDate(e.target.value)
+                                } else {
+                                    let newStartDate = exampleBooking.startDate > endDate ? today : exampleBooking.startDate
+                                    e.target.value = newStartDate
+                                    setStartDate(newStartDate)
+                                }
+                                }}>
+                            </input>                        
                         </div>
                         <div className="input">
                             <label className="form-label">End</label>
-                            <input type="date" defaultValue={exampleBooking.endDate}></input>
+                            <input type="date" defaultValue={endDate} required="required" min={addDay(startDate)} onChange={(e) => {
+                                if(e.target.value!=="") {
+                                    setEndDate(e.target.value)
+                                } else {
+                                    e.target.value = addDay(startDate)
+                                    setEndDate(addDay(startDate))
+                                }
+                                }}>
+                            </input>
                         </div>
                         <div className="input">
                             <label className="form-label">Name</label>
@@ -47,7 +71,7 @@ export function Checkout() {
                 <div className='formRow'>
                     <div className="input">
                         <label className="form-label">Room Size</label>
-                        <select name='roomSize' id='roomSize'>
+                        <select name='roomSize' id='roomSize' defaultValue={roomSize} onChange={(e) => setRoomSize(e.target.value)}>
                             <option value='small'>Small</option>
                             <option value='medium'>Medium</option>
                             <option value='large'>Large</option>
@@ -55,7 +79,7 @@ export function Checkout() {
                     </div>
                     <div className="input">
                         <label className="form-label">Number of Adults</label>
-                        <input type="number" min={0} defaultValue={numberAdults} onChange={(e) => setNumberAdults(parseInt(e.target.value))}></input>
+                        <input type="number" min={1} defaultValue={numberAdults} onChange={(e) => setNumberAdults(parseInt(e.target.value))}></input>
                     </div>
                     <div className="input">
                         <label className="form-label">Number of Children</label>
@@ -65,12 +89,36 @@ export function Checkout() {
             </div>
         </div>
         <div className='confirm'>
-            <div style={{paddingRight:".5rem"}}>Total Booking Cost: ${exampleBooking.baseRate * (numberAdults + (numberChildren * .5)) * diffInDays}</div>
+            <div style={{paddingRight:".5rem"}}>Total Booking Cost: ${totalCost}</div>
             <button type="submit">
-                Search
+                Continue
             </button>
         </div>
     </div>)
+}
+
+function addDay(dateTime) {
+    let date = new Date(dateTime);
+
+    // Add one day to the date
+    date.setDate(date.getDate() + 1);
+
+    // Convert the date back to a string
+    let newDateString = date.toISOString().split('T')[0];
+
+    return newDateString
+}
+
+function minusDay(dateTime) {
+    let date = new Date(dateTime);
+
+    // Add one day to the date
+    date.setDate(date.getDate() - 1);
+
+    // Convert the date back to a string
+    let newDateString = date.toISOString().split('T')[0];
+
+    return newDateString
 }
 
 export default Checkout;
