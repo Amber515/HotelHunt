8e8f474ebbeefec6595c5b985dd8818e24b318ac
Anastasiia Efimova <nastkaefff@mail.ru>
2025-03-10 18:26:24 -0400

    Add a Hotel Map

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/home";
 import Checkout from "./components/checkout/checkout";
 import Booking from "./components/booking";
 import History from "./components/history";
+import Map from "./components/map";
 import SearchResults from "./components/searchResults/searchResults";
 import ResetPassword from './components/auth/reset'; // Adjust path as necessary
 
@@ -28,6 +29,10 @@ function App() {
     {
       path: "/login",
       element: <Login />,
+    },
+    {
+      path: "/map",
+      element: <Map />,
     },
     {
       path: "/register",
